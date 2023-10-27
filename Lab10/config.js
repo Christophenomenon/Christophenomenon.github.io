01b404091a695603ad4615ad0d52ebf1f679aceb
Christophenomenon <18801301060@163.com>
2023-10-27 16:30:05 -0400

    Update config.js

--- Lab10/config.js
+++ Lab10/config.js
@@ -9,21 +9,21 @@ var config = {
     inset: true,
     theme: 'dark',
     use3dTerrain: false, //set true for enabling 3D maps.
-    title: 'The Title Text of this Story',
-    subtitle: 'A descriptive and interesting subtitle to draw in the reader',
-    byline: 'By a Digital Storyteller',
+    title: 'Public and Affordable Housing Flood Risk',
+    subtitle: 'Storymap of previous Lab',
+    byline: 'By Hongtao',
     footer: 'Source: source citations, etc. <br> Created using <a href="https://github.com/mapbox/storytelling" target="_blank">Mapbox Storytelling</a> template.',
     chapters: [
         {
             id: 'slug-style-id',
             alignment: 'left',
             hidden: false,
-            title: 'Display Title',
+            title: 'General Background',
             image: 'images/source.png',
-            description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+            description: 'In the fall of 2012, New York City was hit with hurricane that left tens of thousands of public housing residents without power for days or even weeks]. New York City Housing Authority public housing developments were affected. 24,000 government-subsidized apartments and 40,000 rent-stabilized apartments were also affected.',
             location: {
-                center: [-122.418398, 37.759483],
-                zoom: 8.5,
+                center: [-73.935242, 40.730610],
+                zoom: 12,
                 pitch: 60,
                 bearing: 0
             },
@@ -31,17 +31,16 @@ var config = {
             rotateAnimation: false,
             callback: '',
             onChapterEnter: [
-                // {
-                //     layer: 'layer-name',
-                //     opacity: 1,
-                //     duration: 5000
-                // }
+                {
+                 layer: 'pluto-hous-type-master-featu-7b5u7f',
+                  opacity: 0,
+                }
             ],
             onChapterExit: [
-                // {
-                //     layer: 'layer-name',
-                //     opacity: 0
-                // }
+                {
+                 layer: 'pluto-hous-type-master-featu-7b5u7f',
+                  opacity: 1,
+                }
             ]
         },
         {
