620aacd7888f57d0b1a96d76fc161e5249517c45
Christophenomenon <18801301060@163.com>
2023-10-27 19:15:55 -0400

    Update config.js

--- Lab12/config.js
+++ Lab12/config.js
@@ -12,7 +12,12 @@ var config = {
     title: 'Coastal Flooding in New York City',
     subtitle: 'Assessing Risks to Vulnerable Housing',
     byline: 'By Hongtao',
+
+    para1: 'Coastal Flooding in New York City has become an increasingly urgent concern, driven by the growing impacts of climate change. The  extensive waterfront areas and densely populated neighborhoods are particularly vulnerable to rising sea levels and more frequent and severe storms. With millions of residents and billions of dollars in real estate at risk, understanding the precise vulnerabilities of housing in these areas is crucial. The city has taken significant steps to address this issue, from implementing innovative infrastructure projects to stricter building codes, but more comprehensive risk assessment and mitigation strategies are required to protect vulnerable communities.',
+    para2: 'Assessing Risks to Vulnerable Housing is a critical aspect of addressing this multifaceted challenge. It involves not only identifying which neighborhoods and housing units are most at risk but also developing strategies to protect and, in some cases, relocate residents. Additionally, it necessitates a thorough examination of the socio-economic factors that contribute to housing vulnerability, as low-income communities are often disproportionately affected. Public policy, investment in resilient infrastructure, and community engagement are all essential components of mitigating the risks associated with coastal flooding in New York City. To safeguard the future of the city and ensure the well-being of its residents, a proactive and holistic approach to addressing this issue is imperative.',
+    
     footer: 'Source: source citations, etc. <br> Created using <a href="https://github.com/mapbox/storytelling" target="_blank">Mapbox Storytelling</a> template.',
+    video: 'images/902-1_902-2364-PD2_preview.mp4',
     chapters: [
         {
             id: 'first-chapter',
@@ -68,6 +73,18 @@ var config = {
             callback: '',
             onChapterEnter: [],
             onChapterExit: []
+        },
+        {
+            id: 'interlude',
+            alignment: 'center',
+            hidden: false,
+            description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam ultricies tempor purus, eget tempus sem bibendum vel. Maecenas at justo non felis accumsan condimentum vitae et leo. Sed sodales nec risus maximus congue. Ut quis tincidunt leo. Pellentesque sapien nisl, congue tempus mauris in, pulvinar mattis turpis. Nullam sit amet justo egestas, laoreet mauris ut, pulvinar ipsum. Pellentesque vehicula imperdiet iaculis. Nam in tellus metus. Donec a est non orci efficitur ornare id mattis eros. Aliquam vel facilisis ligula. Nullam tincidunt, sapien vitae bibendum consequat, nibh eros eleifend ligula, et aliquam enim justo eget felis. Sed nec vehicula metus. Pellentesque rutrum dui quis egestas hendrerit. Nullam auctor et tortor sit amet congue.',
+            location: {
+                center: [-73.97615, 40.71901],
+                zoom: 14.84,
+                pitch: 48,
+                bearing: 142.44,
+            },
         },
         {
             id: 'third-chapter',
