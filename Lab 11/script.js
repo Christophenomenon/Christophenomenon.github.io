8490fc47ab68a2f6842b60db0b21a67102b55884
Christophenomenon <18801301060@163.com>
2023-10-30 12:04:47 -0400

    Update script.js

--- Lab 11/script.js
+++ Lab 11/script.js
@@ -1,9 +1,11 @@
 mapboxgl.accessToken = 'pk.eyJ1IjoiaGg3NTgiLCJhIjoiY2xuMXo5bjQxMDJ5YzJrbGpvbXZudTVudSJ9.SzlG3WInkn7aWBEYPJkw0A'
 const map = new mapboxgl.Map({
-  container: 'map', // container id
+  container: 'Hongtao', // container id
   style: 'mapbox://styles/hh758/clo5vasc200hp01pd9cts456i', // replace this with your style URL,
-  center: [-73.935242, 40.730610], // The convention for coordinates is  typically [long, lat]
-  zoom: 12 // The zoom level can be any decimal number
+  center: [-122.42285, 37.76869], // The convention for coordinates is  typically [long, lat]
+  zoom: 12,// The zoom level can be any decimal number
+  pitch: 50,
+  bearing: -40
 });
 map.on('load',function(){  
 });
