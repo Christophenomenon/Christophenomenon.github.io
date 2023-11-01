e850eb5df03c5dd1d3518f9078b288d5ed926a2a
Christophenomenon <18801301060@163.com>
2023-11-01 12:55:25 -0400

    Update script.js

--- Lab11/script.js
+++ Lab11/script.js
@@ -3,7 +3,7 @@ const map = new mapboxgl.Map({
   container: 'Hongtao', // container id
   style: 'mapbox://styles/hh758/clo5vasc200hp01pd9cts456i', // replace this with your style URL,
   center: [-122.42285, 37.76869], // The convention for coordinates is  typically [long, lat]
-  zoom: 12,// The zoom level can be any decimal number
+  zoom: 13,// The zoom level can be any decimal number
   pitch: 50,
   bearing: -40
 });
